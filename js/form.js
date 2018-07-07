a03f883bfab9ad066379df16384352a48ec242a9
Deniel Castro <deniel.castro@icloud.com>
2018-07-07 01:31:39 -0700

    v2.00

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
-// JavaScript Documentvar x = document.getElementById("form_sample");
+// JavaScript Document
+var x = document.getElementById("form_sample");
 var createform = document.createElement('form'); // Create New Element Form
 createform.setAttribute("action", ""); // Setting Action Attribute on Form
 createform.setAttribute("method", "post"); // Setting Method Attribute on Form
